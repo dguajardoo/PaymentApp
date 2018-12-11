2acb4fd492a5c5d221021d50dd68dd43aede5cbe
David <david.guajardo.o@gmail.com>
2018-12-10 22:02:29 -0300

    Initial Commit

---
+++ PaymentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by David Guajardo Osorio on 12/10/18.
+//  Copyright © 2018 David Guajardo Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
