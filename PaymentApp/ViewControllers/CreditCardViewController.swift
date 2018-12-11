bf2f954e253c7ecc2690f4a42ddc57debc709e87
David <david.guajardo.o@gmail.com>
2018-12-10 23:51:10 -0300

    Diseño segunda vista

--- PaymentApp/ViewControllers/CreditCardViewController.swift
+++ PaymentApp/ViewControllers/CreditCardViewController.swift
@@ -8,17 +8,68 @@
 
 import UIKit
 
-class CreditCardViewController: UIViewController {
+class CreditCardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableCreditCard: UITableView!
+    
     var monto: String? = ""
+    
+    var arr = ["dfsd","sdfsdf", "sdfsdfsdf", "s"]
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tableCreditCard.delegate = self
+        self.tableCreditCard.dataSource = self
     }
     
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // number of rows in table view
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.arr.count
+        
+    }
+    
+    
+    
+    // create a cell for each table view row
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        // create a new cell if needed or reuse an old one
+        
+        let cell:UITableViewCell = self.tableCreditCard.dequeueReusableCell(withIdentifier: "MyCell") as UITableViewCell!
+        
+        
+        // set the text from the data model
+        
+        cell.textLabel?.text = self.arr[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    
+    // method to run when table view cell is tapped
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("You tapped cell number \(indexPath.row).")
+        
+    }
+    
     /*
     // MARK: - Navigation
 
